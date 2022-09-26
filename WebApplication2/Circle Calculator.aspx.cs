e59c2340841436a13fe316e90c4990df97799c2b
shawn <shawnfeng610@gmail.com>
2022-09-26 15:51:18 +0800

    add WebApp2 with Circle class

---
+++ WebApplication2/Circle Calculator.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class Circle_Calculator : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Circle c = new Circle();
+                c.Radius = Double.Parse(TextBox1.Text);
+                Label1.Text = c.Area().ToString();
+                Label2.Text = c.Circumferece().ToString();
+            }
+            catch(Exception err)
+            {
+                Label1.Text = err.Message.ToString();
+            }
+
+        }
+    }
+}
---
+++ WebApplication2/Circle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2
+{
+    public class Circle
+    {
+        private double radius;
+        public double Radius
+        {
+            get
+            {
+                return radius;
+            }
+            set
+            {
+                radius = value;
+            }
+        }
+
+        public double Area()
+        {
+            return Math.PI * radius * radius;
+        }
+
+        public double Circumferece()
+        {
+            return Math.PI * 2 * radius;
+        }
+    }
+}
