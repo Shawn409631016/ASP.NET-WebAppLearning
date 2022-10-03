69f0eef02ddafa3fda943cbdf45c51c05ceffdce
shawn <shawnfeng610@gmail.com>
2022-10-03 16:06:23 +0800

    finish checkTime

---
+++ checkTime/WebForm2.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace checkTime
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int c;
+            if (this.ViewState["counter"] == null)
+            {
+                c = 1;
+            }
+            else
+            {
+                c = (int)this.ViewState["counter"] + 1;
+            }
+            this.ViewState["counter"] = c;
+            Label1.Text = c.ToString();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            this.Response.Redirect("WebForm3");
+        }
+    }
+}
