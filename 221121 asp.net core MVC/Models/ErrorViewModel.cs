1c7029fd00fd0ee4ff2e124522a0683bc6587901
shawn <shawnfeng610@gmail.com>
2022-11-22 01:03:24 +0800

    add 222122 asp.net core

--- 221121 asp.net core MVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace _221121_asp.net_core_MVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
