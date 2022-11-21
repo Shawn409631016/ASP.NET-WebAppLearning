08aa1ab59461ee625f64e5b068d7bb610eb412d5
shawn <shawnfeng610@gmail.com>
2022-11-22 01:27:30 +0800

    add 221122 asp.net core webapp

---
+++ 221122 asp.net core webapp/221122 asp.net core webapp/Controllers/WelcomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _221122_asp.net_core_webapp.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult PostData(string value1, string value2)
+        {
+            ViewData["Result"] = Int32.Parse(value1) + Int32.Parse(value2);
+            return View();
+        }
+
+    }
+}
---
+++ 221122 asp.net core webapp/221122 asp.net core webapp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _221122_asp.net_core_webapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
